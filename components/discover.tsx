fcd4a8c81c6277bf16b90d3a5de133af6acf56bb
mselbekk11 <mselbekk11@gmail.com>
2026-02-09 16:24:06 -0800

    updates

--- components/discover.tsx
+++ components/discover.tsx
@@ -39,18 +39,20 @@ export default function Discover() {
   ];
 
   return (
-    <div className='bg-slate-900 max-w-5xl mx-auto'>
-      <div>
-        <div className='flex justify-between items-center'>
-          <div>Discover</div>
-          <div>Toggle</div>
-        </div>
-        <div className='grid grid-cols-4 gap-4 min-h-[500px]'>
-          {categories.map((category) => (
-            <div key={category.id} className='bg-slate-800 rounded-lg p-4'>
-              {/* <img src={category.img} alt={category.title} /> */}
-            </div>
-          ))}
+    <div className='bg-zinc-900'>
+      <div className=' max-w-7xl mx-auto py-20'>
+        <div>
+          <div className='flex justify-between items-center mb-4'>
+            <div className='text-white'>Discover</div>
+            <div className='text-white'>Toggle</div>
+          </div>
+          <div className='grid grid-cols-4 gap-4 min-h-[300px]'>
+            {categories.map((category) => (
+              <div key={category.id} className=' rounded-lg p-4 bg-white'>
+                {/* <img src={category.img} alt={category.title} /> */}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
