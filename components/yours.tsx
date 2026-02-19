3e66e2513a5bcb49f2440f39f69372c9bc4362b5
mselbekk11 <mselbekk11@gmail.com>
2026-02-18 22:39:33 -0800

    updates

--- components/yours.tsx
+++ components/yours.tsx
@@ -29,7 +29,7 @@ const items = [
 
 export default function Yours() {
   return (
-    <div className='bg-[#FAFAFA] py-16 md:py-30 border-b border-zinc-300 relative'>
+    <div className='bg-[#FAFAFA] py-16 md:py-30 border-b border-zinc-300 relative overflow-hidden'>
       <div className='absolute -bottom-[400px] -right-[400px] w-[800px] h-[800px] rounded-full border-[60px] border-[#FF3800] opacity-5 pointer-events-none hidden lg:block' />
       <div className='mx-auto max-w-[1240px] px-4 relative z-0'>
         {/* Intro */}
